330ddfa2f01fb492d9e336c9d82a8fdd7b610a89
Vitaly <v.kononov@chillicode.ru>
2022-11-30 22:30:14 +0300

    feat: installed husky and firebase

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBuSCD0zpyvb506fBzPQiipu3aFDBv0hJQ',
+  authDomain: 'netflix-clone-79e6b.firebaseapp.com',
+  projectId: 'netflix-clone-79e6b',
+  storageBucket: 'netflix-clone-79e6b.appspot.com',
+  messagingSenderId: '366857962492',
+  appId: '1:366857962492:web:ef7be1091c157a2463e879',
+};
